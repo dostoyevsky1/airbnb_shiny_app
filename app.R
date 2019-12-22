da5461810c0e0447dc0d9c60b445e43c529b75d5
dostoyevsky1 <mdrozdov94@gmail.com>
2019-12-22 00:04:29 -0500

    nyc airbnb exploration shiny app

---
+++ app.R
@@ -0,0 +1,116 @@
+
+source('global.R')
+
+
+
+vars = c('Price' = 'price',
+         'Borough' = 'neighbourhood_group',
+         'Neighborhood' = 'neighbourhood',
+         'Monthly Reviews' = 'reviews_per_month',
+         'Total Reviews' = 'number_of_reviews',
+         'Minimum Nights' = 'minimum_nights',
+         'Room Type' = 'room_type')
+
+vars2 = c('None' = 'none',
+          'Price' = 'price',
+          'Monthly Reviews' = 'reviews_per_month',
+          'Total Reviews' = 'number_of_reviews')
+
+ui = navbarPage("NYC AirBnB", id="nav",
+           
+           tabPanel("Interactive map",
+                    div(class="outer",
+                        
+                        tags$head(
+                            # Include our custom CSS
+                            includeCSS("./styles.css")
+                        ),
+                        
+                        # If not using custom CSS, set height of leafletOutput to a number instead of percent
+                        leafletOutput("map", width="100%", height="100%"))),
+           
+           absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                         draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                         width = 400, height = "auto",
+                         
+                         h2("NYC AirBnB Explorer"),
+                         
+                         selectInput("color", "Color", vars, selected = 'price'),
+                         selectInput("size", "Size", vars2, selected = 'none'),
+                         plotOutput("p_hist", height = 250, width = 350)
+                         ))
+           
+                        
+
+# Define server logic required to draw a histogram
+server = function(input, output, session) {
+    
+    map = leaflet() %>% 
+        addProviderTiles('MapBox',
+                         options = providerTileOptions(id = 'mapbox.dark',noWrap=FALSE,
+                                                       accessToken = 'pk.eyJ1IjoibWRyaXp6eSIsImEiOiJjazRnN3R6N2Qwd2tlM2xtdzl4Z3J5ajFxIn0.aC1WFSuBLtK6aWQdvnIRsg'
+                         )) %>% 
+        setView(lng = -73.9855, lat = 40.7000, zoom = 12)
+    
+    observe({
+        
+        colorBy = input$color
+        sizeBy = input$size
+    
+        if(colorBy == 'price'){
+            bnbdat = bnb
+            pal = colorFactor('YlGn', bnbdat[,colorBy])
+        } else if(colorBy == 'number_of_reviews' |
+                  colorBy == 'minimum_nights') {
+            bnbdat = bnb
+            pal = colorFactor('Reds', bnbdat[,colorBy])
+        } else if(colorBy == 'reviews_per_month'){
+            bnbdat = na.omit(bnb)
+            pal = colorFactor('Reds', bnbdat[,colorBy])
+        } else {
+            bnbdat = bnb
+            pal = colorFactor('Accent', bnbdat[,colorBy])
+        }
+        
+        
+        if(sizeBy == 'price'){
+            
+            radius = bnbdat$p_radius   
+            
+        } else if(sizeBy == 'reviews_per_month') {
+            
+            radius = bnbdat$r_radius
+        
+        } else if(sizeBy == 'number_of_reviews'){
+            
+            radius = bnbdat$b_radius
+            
+        } else if(sizeBy == 'none') {
+            radius = 2
+        }
+        
+        
+        leafletProxy("map", data = bnbdat) %>%
+            clearMarkers() %>%
+            addCircleMarkers(~longitude, ~latitude, radius=radius,fillOpacity = 2, 
+                             fillColor = ~pal(bnbdat[,colorBy]), stroke = FALSE,
+                             label = ~paste(paste0('Price: ','$',as.character(price)),
+                                             paste('Neighborhood: ', as.character(neighbourhood)),
+                                             paste('Minimum Nights: ', as.character(minimum_nights)),
+                                             paste('Room Type: ', as.character(room_type)), sep = ' / '))
+        
+        p_hist = ggplot() + geom_density(aes(x=bnbdat[,colorBy]), fill = '#00DD00') + ggtitle(paste0('Distribution of ',colorBy)) + xlab(colorBy) + ylab('Density')
+        output$p_hist = renderPlot({p_hist})
+        
+    })
+    
+    output$map = renderLeaflet({map})
+    
+    
+        
+        
+        
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ global.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(ggplot2)
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+
+
+
+bnb = read.csv('./AB_NYC_2019.csv')
+
+bnb = bnb %>% mutate(p_radius = ifelse(price <= 100, 1,
+                                       ifelse(
+                                         price <= 500, 2, ifelse(price <= 1000, 10, ifelse(
+                                           price <= 3000, 20, ifelse(price <= 10000, 30, price)
+                                         ))
+                                       ))) %>%
+  mutate(r_radius = ifelse(
+    reviews_per_month <= 1,
+    1,
+    ifelse(
+      reviews_per_month <= 5,
+      2,
+      ifelse(
+        reviews_per_month <= 10,
+        10,
+        ifelse(reviews_per_month <= 58.5, 15, reviews_per_month)
+      )
+    )
+  )) %>%
+  mutate(b_radius = ifelse(
+    number_of_reviews <= 100,
+    1,
+    ifelse(
+      number_of_reviews <= 200,
+      2,
+      ifelse(
+        number_of_reviews <= 300,
+        3,
+        ifelse(number_of_reviews <= 629, 15, number_of_reviews)
+      )
+    )
+  ))
+
